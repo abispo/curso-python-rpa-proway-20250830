704c51309cd7df50e3aa64e34edb3cda2f559dc4
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-09-05 17:36:20 -0300

    Nova estrutura

--- 20250906-aula02/01_envio_de_email.py
+++ 20250906-aula02/01_envio_de_email.py
@@ -14,59 +14,30 @@ def criar_banco_de_dados(conn: Connection):
     cursor = conn.cursor()
 
     sql = """
-    CREATE TABLE IF NOT EXISTS sensores(
+    CREATE TABLE IF NOT EXISTS leituras_sensores(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        codigo TEXT UNIQUE NOT NULL
+        codigo_sensor TEXT NOT NULL,
+        leitura_chuva TEXT NOT NULL,
+        leitura_temperatura TEXT NOT NULL,
+        leitura_umidade TEXT NOT NULL,
+        data_hora TEXT NULL
     );"""
-    cursor.execute(sql)
-
-    sql = """CREATE TABLE IF NOT EXISTS leituras_chuva(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        sensor_id INTEGER NOT NULL,
-        valor TEXT NULL,
-        FOREIGN KEY(sensor_id) REFERENCES sensores(id)
-    );"""
-    cursor.execute(sql)
-
-    sql = """CREATE TABLE IF NOT EXISTS leituras_temperatura(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        sensor_id INTEGER NOT NULL,
-        valor TEXT NULL,
-        FOREIGN KEY(sensor_id) REFERENCES sensores(id)
-    );"""
-    cursor.execute(sql)
-
-    sql = """CREATE TABLE IF NOT EXISTS leituras_umidade(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        sensor_id INTEGER NOT NULL,
-        valor TEXT NULL,
-        FOREIGN KEY(sensor_id) REFERENCES sensores(id)
-    );
-    """
     cursor.execute(sql)
 
 def salvar_dados_na_tabela(conn: Connection, nome_arquivo: str, arquivo_csv: csv.DictReader):
     
-    codigo_sensor, tipo_leitura, _ = nome_arquivo.split("_")
     cursor = conn.cursor()
 
-    sql = f"SELECT id, codigo FROM sensores"
-    result = cursor.execute(sql).fetchall()
-
-    dicionario_sensores = {}
-
-    for item in result:
-        dicionario_sensores.update({item[1]: item[0]})
-
     for linha in arquivo_csv:
-        if codigo_sensor not in dicionario_sensores.keys():
-            cursor.execute(f"INSERT INTO sensores(codigo) VALUES (?)", (codigo_sensor,))
-            conn.commit()
-            dicionario_sensores.update({codigo_sensor: cursor.lastrowid})
-
-        sensor_id = dicionario_sensores.get(codigo_sensor)
-        sql = f"INSERT INTO leituras_{tipo_leitura}(sensor_id, valor) VALUES (?, ?)"
-        cursor.execute(sql, (sensor_id, linha.get('valor')))
+        sql = f"""
+            INSERT INTO leituras_sensores(
+                codigo_sensor,
+                data_hora,
+                leitura_chuva,
+                leitura_temperatura,
+                leitura_umidade
+            ) VALUES (?, ?, ?, ?, ?)"""
+        cursor.execute(sql, tuple(linha.values()))
         conn.commit()
         
 def gerar_planilha(conn: Connection):
@@ -75,29 +46,27 @@ def gerar_planilha(conn: Connection):
     cursor = conn.cursor()
     wb.remove(wb.active)
 
-    result = cursor.execute("SELECT codigo FROM sensores ORDER BY codigo").fetchall()
-    for codigo in result:
-        ws = wb.create_sheet(title=codigo[0])
+    result = cursor.execute("SELECT DISTINCT codigo_sensor FROM leituras_sensores").fetchall()
 
-        ws.append(["chuva", "temperatura", "umidade"])
+    for sensor in result:
+        ws = wb.create_sheet(title=sensor[0])
+        ws.append(["data_hora", "chuva", "temperatura", "umidade"])
+        ws.insert_rows(2, 1)
 
         sql = """
             SELECT
-                s.codigo AS sensor,
-                c.valor AS chuva,
-                t.valor AS temperatura,
-                u.valor AS umidade
-            FROM sensores s
-            INNER JOIN leituras_chuva c
-                ON s.id = c.sensor_id
-            INNER JOIN leituras_temperatura t
-                ON s.id = t.sensor_id
-            INNER JOIN leituras_umidade u
-                ON s.id = u.sensor_id
-            ORDER BY s.codigo;"""
-        result = cursor.execute(sql).fetchall()
+                leitura_chuva,
+                leitura_temperatura,
+                leitura_umidade,
+                data_hora
+            FROM leituras_sensores
+            WHERE codigo_sensor = ?"""
 
-        print(result)
+        result = cursor.execute(sql, sensor).fetchall()
+
+        for linha in result:
+            ws.append(linha)
+            print(f"Dados para o sensor {sensor} gerados com sucesso.")
 
     wb.save(os.path.join(os.getcwd(), "dados_sensores.xlsx"))
 
@@ -112,16 +81,13 @@ if __name__ == "__main__":
 
     criar_banco_de_dados(conexao)
     
-    pasta_arquivos = os.path.join(os.getcwd(), "sensores_dia")
+    arquivo_leituras = os.path.join(os.getcwd(), "leituras.csv")
     arquivo_zip = f"sensores_data_{datetime.now().strftime('%Y%m%d')}"
 
     arquivos: List[str] = []
 
-    for caminho, _, arquivos in os.walk(pasta_arquivos):
-        for arquivo in arquivos:
-            with open(os.path.join(caminho, arquivo), 'r', encoding="utf-8") as _arquivo:
-
-                arquivo_csv = csv.DictReader(_arquivo, delimiter=';')
-                salvar_dados_na_tabela(conexao, arquivo, arquivo_csv)
+    with open(arquivo_leituras, 'r', encoding="utf-8") as _arquivo:
+        arquivo_csv = csv.DictReader(_arquivo, delimiter=';')
+        salvar_dados_na_tabela(conexao, arquivo_leituras, arquivo_csv)
 
     gerar_planilha(conn=conexao)
