bc5a550d550190c0f02fba6a73d1b5bb039343e5
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-09-20 08:23:39 -0300

    Arquivos aula 03 rpa com Python

---
+++ 20250906-aula02/exercicio01.py
@@ -0,0 +1,21 @@
+import csv
+import os
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://defesacivil.riodosul.sc.gov.br/index.php?r=externo%2Fmetragem"
+
+def main():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'DNT': '1',  # Do Not Track
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
+    }
+
+if __name__ == "__main__":
+    main()
