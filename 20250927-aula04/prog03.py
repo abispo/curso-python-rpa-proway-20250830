ad67f84b8d353a04833e42115d340839de75e865
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-09-27 09:52:11 -0300

    Arquivos aula 04 automações com Python

---
+++ 20250927-aula04/prog03.py
@@ -0,0 +1,49 @@
+# Localizando elementos na página
+
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = ChromeOptions()
+options.page_load_strategy = "eager"
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome(
+        service=ChromeService(
+            ChromeDriverManager().install()
+        ),
+        options=options
+    )
+
+    # Encontrando um elemento pelo id
+    driver.get("https://www.proway.com.br/")
+
+    text_busca = driver.find_element(By.ID, "termoBuscaCurso")
+    sleep(1)
+    text_busca.send_keys("Python")
+    text_busca.send_keys(Keys.ENTER)
+
+    link_moda_textil = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Moda e Têxtil"))
+    )
+
+    link_moda_textil.click()
+    
+    WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.ID, "listaCursos"))
+    )
+
+    cursos_moda_textil = driver.find_elements(By.XPATH, "//div[@class='sombra']//h2")
+    print("=== Cursos na área de Moda e Têxtil ===")
+
+    for curso in cursos_moda_textil:
+        print(f"* {curso.text}.")
+
+    driver.quit()
