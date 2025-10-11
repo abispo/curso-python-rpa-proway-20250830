1b55709f4a19dfb1e115e3928d3868dbf38e5fa4
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-10-11 11:36:53 -0300

    Arquivos aula 06 rpa com Python

--- 20251011-aula06/index.js
+++ 20251011-aula06/index.js
@@ -1,5 +1,10 @@
+import express from 'express';
 import QRCode from 'qrcode'
 import makeWASocket, { DisconnectReason, useMultiFileAuthState } from "baileys";
+
+const app = express()
+
+let latestQRCode = null;
 
 const start = async() => {
     const { state, saveCreds } = await useMultiFileAuthState('sessions')
@@ -7,11 +12,9 @@ const start = async() => {
 
     sock.ev.on('connection.update', async (update) => {
         const {connection, lastDisconnect, qr } = update
-        // on a qr event, the connection and lastDisconnect fields will be empty
       
-        // In prod, send this string to your frontend then generate the QR there
         if (qr) {
-          // as an example, this prints the qr code to the terminal
+          latestQRCode = qr
           console.log(await QRCode.toString(qr, {type:'terminal', small: true}))
         }
 
@@ -35,13 +38,13 @@ const start = async() => {
         if (m.type === 'notify') {
             const msg = m.messages[0]
             
-            if (!msg.key.fromMe && !msg.key.remoteJid.includes('@g.us')) {
+            if (!msg.key.fromMe && !msg.key.remoteJid.includes('@g.us') || !msg.key.remoteJid === 'status@broadcast') {
                 const phone = msg.key.remoteJid
-                const textMessage = msg.key.conversation
+                const textMessage = msg.message.conversation
 
                 console.log('Mensagem recebida: "' + textMessage + '"')
 
-                await sock.sendPresenceUpdate('recording', phone)
+                await sock.sendPresenceUpdate('composing', phone)
                 await new Promise(r => setTimeout(r, 3000))
 
                 await sock.sendMessage(phone, { text: "Mensagem recebida de '" + phone + "'."})
@@ -53,4 +56,33 @@ const start = async() => {
       sock.ev.on('creds.update', saveCreds)
 }
 
-start().catch(console.error)
+app.get('/qr', async (req, res) => {
+    if (!latestQRCode) {
+        return res.status(404).send('O QRCode ainda não foi gerado. Por favor aguarde...')
+    }
+
+    try {
+        const svgString = await QRCode.toString(latestQRCode, {type: 'svg'})
+
+        const html = `
+        <html>
+            <head><title>Automação Proway WhatsApp QRCode</title></head>
+            <body style="display: flex; justify-content: center; align-items: center, height: 100vh, flex-direction: column;">
+            <h1>Escaneie o seguinte código para conectar o seu whatsapp</h1>
+            ${svgString}
+        
+        `
+
+        res.setHeader('Content-type', 'text/html')
+        res.send(html)
+    } catch (err) {
+        res.status(500).send('Erro ao gerar o QRCode: ' + err.text)
+    }
+})
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado!')
+    start().catch(console.error)
+})
+
+// start().catch(console.error)
