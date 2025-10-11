3eca95bdf67ae2c5f53dffe26877f74c5a29326a
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-10-11 12:26:05 -0300

    Arquivos aula 06 rpa com Python

---
+++ 20251011-aula06/prog02_telegrambot_pooling.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+from telegram import Update, Bot
+from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
+from asyncio import Queue
+
+import os
+import datetime
+
+load_dotenv()
+
+class AutomacaoProwayBot(Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(token=os.getenv("TELEGRAM_TOKEN"))
+
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text(f"Olá! Eu sou o bot telegram da turma RPA com Python! ({datetime.datetime.now()})")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().bot(AutomacaoProwayBot()).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
